bab406dadf0761497f2552a16799a1ff154caabd
Arjun Reddy <70253095+Arjunreddypulugu@users.noreply.github.com>
2025-03-28 12:19:05 -0400

    Update utils.py

--- test_timeclock/utils.py
+++ test_timeclock/utils.py
@@ -6,6 +6,6 @@ def find_customer_from_location(lat, lon, conn):
     """)
     rows = cursor.fetchall()
     for row in rows:
-        if row.MinLat <= lat <= row.MaxLat and row.MinLon <= lon <= row.MaxLon:
+        if row.MinLat <= lat <= row.MaxLat and row.MinLon >= lon >= row.MaxLon:
             return row.CustomerName
     return None
