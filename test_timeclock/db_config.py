6733b87a781489abcd644835cfb7120fad52ae96
Arjun Reddy <70253095+Arjunreddypulugu@users.noreply.github.com>
2025-04-03 09:44:46 -0400

    Update db_config.py

--- test_timeclock/db_config.py
+++ test_timeclock/db_config.py
@@ -1,15 +1,28 @@
+# db_config.py
 import pyodbc
 import streamlit as st
 
-@st.cache_resource
+class DBConnection:
+    def __init__(self):
+        self.conn = None
+        
+    def __enter__(self):
+        db = st.secrets["database"]
+        connection_string = (
+            f"Driver={{{db['driver']}}};"
+            f"Server={db['server']};"
+            f"Database={db['database']};"
+            f"UID={db['username']};"
+            f"PWD={db['password']};"
+            f"MARS_Connection=Yes;"
+        )
+        self.conn = pyodbc.connect(connection_string)
+        return self.conn
+        
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.conn:
+            self.conn.close()
+
+@st.cache_resource(ttl=300, max_entries=20)  # Recycle connections every 5 minutes
 def get_connection():
-    db = st.secrets["database"]
-    connection_string = (
-        f"Driver={{{db['driver']}}};"
-        f"Server={db['server']};"
-        f"Database={db['database']};"
-        f"UID={db['username']};"
-        f"PWD={db['password']};"
-        f"MARS_Connection=Yes;"  # Enable MARS
-    )
-    return pyodbc.connect(connection_string)
+    return DBConnection()
