ca0fb9a46f85041c87ddec40f0e38f8c04ce15ea
Arjun Reddy <70253095+Arjunreddypulugu@users.noreply.github.com>
2025-04-03 10:34:48 -0400

    Update utils.py

--- test_timeclock/utils.py
+++ test_timeclock/utils.py
@@ -5,11 +5,11 @@ def find_customer_from_location(lat, lon):
     with get_connection() as conn:  # Use context manager
         cursor = conn.cursor()
         cursor.execute("""
-            SELECT CustomerName, MinLat, MaxLat, MinLon, MaxLon
+            SELECT customer_name, min_latitude, max_latitude, min_longitude, max_longitude
             FROM LocationCustomerMapping
         """)
         rows = cursor.fetchall()
         for row in rows:
-            if row.MinLat <= lat <= row.MaxLat and row.MinLon >= lon >= row.MaxLon:
-                return row.CustomerName
+            if row.min_latitude <= lat <= row.max_latitude and row.min_longitude >= lon >= row.max_longitude:
+                return row.customer_name
         return None
